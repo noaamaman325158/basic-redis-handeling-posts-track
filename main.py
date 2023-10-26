0c3c9ab6253be9e383e2edc17f02ce5c94d7cb49
noaa <noaamaman325158@gmail.com>
2023-10-27 00:20:12 +0300

    Track with redis about the latest posts and inputs. Working with key value data structure and list data structures inside the redis database.

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import redis
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+
+r = redis.Redis()
+
+# Create some secret key for working with the flask module
+app.secret_key = 'key'
+
+last_id = 0
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    global last_id
+    if request.method == "POST":
+        req = request.form
+        name = req["full_name"]
+        post = req["data"]
+        last = r.get("last_id")
+        if last is None:
+            last_id = 1
+        else:
+            last_id = int(last)
+            last_id += 1
+        r.set(f"news:name:{last_id}", name)
+        r.set(f"news:post:{last_id}", post)
+        r.set("last_id", last_id)
+        # List
+        r.lpush("post_id", last_id)
+        flash("Successfully submitted the post", category="success")
     return render_template("home.html")
 
 
